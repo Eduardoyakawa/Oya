31f28fda66cd8ebe94086774a557316c2881df5f
Eduardoyakawa <80008704+Eduardoyakawa@users.noreply.github.com>
2021-03-03 19:37:41 -0300

    Add files via upload

---
+++ Soma.c
@@ -0,0 +1,9 @@
+int main(int argc, char *argv[]) {
+int num1, num2;
+printf("Vamos fazer uma soma! \n");
+printf("Digite um valor: ");
+scanf("%i", & num1);
+printf("Digite um valor: ");
+scanf("%i", & num2);
+printf("\nResultado: %i", num1+num2);
+}
